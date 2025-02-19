a30f763496f907e684fdb5d3c31818d72b4b08be
Fraankz <frankq002@gmail.com>
2025-02-19 17:58:11 +0100

    Update TarjetaRFID.java

--- Codigo/src/TarjetaRFID.java
+++ Codigo/src/TarjetaRFID.java
@@ -1,2 +1,10 @@
 public class TarjetaRFID {
+	public class TarjetaRFID extends Credencial {
+    private String numeroSerie;
+
+    public TarjetaRFID(String numeroSerie) {
+        this.tipo = "TarjetaRFID";
+        this.numeroSerie = numeroSerie;
+    }
+}
 }
