8f3912707dd017423252818815b66b0d7778717b
Eplam <eplamasir@protonmail.com>
2025-02-19 19:38:04 +0100

    Commit Clase Biometria (Implementar clase abstracta Credencial en base a esta)

--- Codigo/src/Biometria.java
+++ Codigo/src/Biometria.java
@@ -1,2 +1,39 @@
-public class Biometria {
+public class Biometria extends Credencial {
+
+    private String tipoHuella;
+
+    public Biometria(String tipo, String tipoHuella) {
+        super(tipo);
+        this.tipoHuella = tipoHuella;
+    }
+
+    public String getTipoHuella() {
+        return tipoHuella;
+    }
+
+    public void setTipoHuella(String tipoHuella) {
+        this.tipoHuella = tipoHuella;
+    }
+
+    public String capturarHuella() {
+        System.out.println("Simulando la captura de huella...");
+        return "HUELLA_SIMULADA";
+    }
+
+    public boolean verificarHuella(String huellaCapturada) {
+        System.out.println("Simulando la verificación de huella...");
+        return huellaCapturada != null && huellaCapturada.equals(tipoHuella);
+    }
+
+    @Override
+    public boolean validar() {
+        String huellaCapturada = capturarHuella();
+        boolean resultado = verificarHuella(huellaCapturada);
+        if (resultado) {
+            System.out.println("Huella verificada con éxito. Credencial válida.");
+        } else {
+            System.out.println("La huella no coincide. Credencial inválida.");
+        }
+        return resultado;
+    }
 }
