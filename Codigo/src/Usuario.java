e371c9279b8de87169231e08ff5da96c4ca70c02
Ali313Hz <114501019+Ali313Hz@users.noreply.github.com>
2025-02-19 18:03:28 +0100

    Usuario_Ali

--- Codigo/src/Usuario.java
+++ Codigo/src/Usuario.java
@@ -1,2 +1,16 @@
 public class Usuario {
+
+    private String nombre;
+    private String ID;
+    private Credencial credencial;
+
+    public Usuario(String nombre, String ID, Credencial credencial) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.credencial = credencial;
+    }
+
+    public void solicitarAcceso(){
+
+    }
 }
